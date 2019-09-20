be32b5cdec6042d87a963eb692a36b2d06de0653
Alex Prunov <matrixsamara@yandex.ru>
2019-09-20 18:26:08 +0400

    continue

--- js/sliderCarousel.js
+++ js/sliderCarousel.js
@@ -8,18 +8,30 @@ class SliderCarousel {
     next,
     prev, 
     position = 0,
-  }) {
+    slidesToShow = 3
+  }){
     this.main = document.querySelector(main);
     this.wrap = document.querySelector(wrap);
     this.slides = document.querySelector(wrap).children;
     this.next = document.querySelector(next);
     this.prev = document.querySelector(prev);
-    this.options = {}
-      position
-  };
+    this.slidesToShow = slidesToShow;
+    this.options = { 
+      position,
+      widthSlide: Math.floor(100 / this.slidesToShow)
+    };      
+  }
+
   init() {
     this.addMyClass();
     this.addStyle();
+
+    if(this.prev && this.next) {
+      this.controlSlider();
+    } else {
+      this.addArrow();
+      this.controlSlider();
+    }
   }
 
   addMyClass() {
@@ -43,11 +55,33 @@ class SliderCarousel {
         will-change: transform;
       }
       .my-slider__item {
-        flex: 0 0 25%;
-        margin: 0 auto;
+        flex: 0 0 ${this.options.widthSlide}%;
+        margin: auto 0;
       }
     `;
 
     document.head.appendChild(style);
   }
+
+    controlSlider(){
+      this.prev.addEventListener('click', this.prevSlider.bind(this));
+      this.next.addEventListener('click', this.nextSlider.bind(this));
+  }
+
+  prevSlider(){
+    --this.options.position;
+    console.log(this.options.position);
+    this.wrap.style.transform = `translateX(${this.options.position * this.options.widthSlide}%)`;
+  }
+
+  nextSlider(){
+    ++this.options.position;
+    console.log(this.options.position);
+    this.wrap.style.transform = `translateX(${this.options.position * this.options.widthSlide}%)`;
+  }
+
+
+    addArrow(){
+
+    }
 }
